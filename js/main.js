b2e9e334b83aa484d1f8ca92f074dc813e5f7dc2
Shabnam <88235352+ShabnamRashidova@users.noreply.github.com>
2022-05-30 12:27:57 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,164 @@
+$(".partners-carousel").owlCarousel({
+  loop: true,
+  dots: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+    1200: {
+      items: 4,
+    },
+  },
+});
+$(".banner-carousel").owlCarousel({
+  loop: true,
+  singleItem: true,
+  dots: false,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    576: {
+      items: 1,
+    },
+    768: {
+      items: 1,
+    },
+    1050: {
+      items: 1,
+    },
+    1400: {
+      items: 1,
+    },
+  },
+});
+$(".vacancy-carousel").owlCarousel({
+  loop: true,
+  singleItem: true,
+  dots: false,
+  margin:20,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    576: {
+      items: 1,
+    },
+    768: {
+      items: 1,
+    },
+    998: {
+      items: 2,
+    },
+    1400: {
+      items: 2,
+    },
+  },
+});
+$(".count").each(function () {
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
+        duration: 3000,
+        easing: "swing",
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        },
+      }
+    );
+});
+$(window).scroll(() => {
+  $(this).scrollTop() >= 120
+    ? $(".general-header").addClass("header-fixed")
+    : $(".general-header").removeClass("header-fixed");
+});
+$(".nav-menu-btn i").on("click", () => {
+  $(".header-mobile-container").addClass("show");
+});
+$(".header-mobile-hide-btn i").on("click", () => {
+  $(".header-mobile-container").removeClass("show");
+});
+$(".drop-btn").on("click", () => {
+  $(".submenu").toggleClass("d-none");
+});
+
+   
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  a.innerHTML+='<i class="fa-solid fa-chevron-down"></i>'
+  x[i].appendChild(a);
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+function closeAllSelect(elmnt) {
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+document.addEventListener("click", closeAllSelect);
